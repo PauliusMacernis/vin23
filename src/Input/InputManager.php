33cebb1d7431cb1f91783724a460a0f979afde9a
Paulius Macernis <paulius@emotion.lt>
2019-07-14 22:09:35 +0300

    Refactoring: regex \s+ instead of explode with one space, other minor fixes on InputManager class.

--- src/Input/InputManager.php
+++ src/Input/InputManager.php
@@ -13,8 +13,6 @@ use Price\PriceInterface;
 use RuntimeException;
 use SplFileObject;
 
-// @TODO: namespaces - we should probably need them all over here and there if we extend the application
-// @TODO: getters, setters in all classes.
 final class InputManager
 {
     private const DATA_COLUMNS_EXPECTED_PER_LINE = 3;
@@ -52,6 +50,27 @@ final class InputManager
         return $this->getFile()->current();
     }
 
+    public function convertTransactionLineToObject(string $input, PriceInterface $shipmentPriceService, DiscountSetContainerInterface $discountSetContainer, DiscountAmountMatrix $discountAmountMatrix): OutputItem
+    {
+        $inputArray = preg_split("/[\s]+/", $input, -1, PREG_SPLIT_NO_EMPTY);
+        $this->validateInputOrThrowException($inputArray, $input);
+
+        [$dateTime, $packageSizeCode, $carrierCode] = $inputArray;
+        $inputItemDateTime = (new StringIsoToObjectDateTime())->transformIsoDateToDateTimeObject($dateTime);
+
+        $this->getTransactionsCountMatrix()->addValue($inputItemDateTime, $carrierCode, $packageSizeCode, $this->getLineNumber());
+
+        return new OutputItem(
+            $inputItemDateTime,
+            $packageSizeCode,
+            $carrierCode,
+            $shipmentPriceService,
+            $discountSetContainer,
+            $discountAmountMatrix,
+            $this->getTransactionsCountMatrix()
+        );
+    }
+
     /**
      * @param string[] $inputArray
      */
@@ -80,19 +99,14 @@ final class InputManager
         return $this->file;
     }
 
-    private function getLineNumber()
-    {
-        return $this->lineNumber;
-    }
-
     private function setLineNumber($lineNumber): void
     {
         $this->lineNumber = $lineNumber;
     }
 
-    private function resetTransactionsCountMatrix(): void
+    private function getLineNumber()
     {
-        $this->transactionsCountMatrix->reset();
+        return $this->lineNumber;
     }
 
     private function setTransactionsCountMatrix(TransactionsCountMatrix $transactionsCountMatrix): void
@@ -100,30 +114,13 @@ final class InputManager
         $this->transactionsCountMatrix = $transactionsCountMatrix;
     }
 
-    public function convertTransactionLineToObject(string $input, PriceInterface $shipmentPriceService, DiscountSetContainerInterface $discountSetContainer, DiscountAmountMatrix $discountAmountMatrix): OutputItem
+    private function getTransactionsCountMatrix(): TransactionsCountMatrix
     {
+        return $this->transactionsCountMatrix;
+    }
 
-        // @TODO: Regex may be better for cases with multiple space separator and so on but most likely a bit slower.
-        $inputArray = explode(' ', trim($input));
-        // @TODO: The value may be exported to the constant. I leave it here because column indexes (0,1,2) are here too.
-
-        $this->validateInputOrThrowException($inputArray, $input);
-
-        // @TODO: Create the new item via factory or something similar.
-        $inputItemDateTime = (new StringIsoToObjectDateTime())->transformIsoDateToDateTimeObject($inputArray[0]);
-        $packageSizeCode = $inputArray[1];
-        $carrierCode = $inputArray[2];
-
-        $this->transactionsCountMatrix->addValue($inputItemDateTime, $carrierCode, $packageSizeCode, $this->getLineNumber());
-
-        return new OutputItem(
-            $inputItemDateTime,
-            $packageSizeCode,
-            $carrierCode,
-            $shipmentPriceService,
-            $discountSetContainer,
-            $discountAmountMatrix,
-            $this->transactionsCountMatrix
-        );
+    private function resetTransactionsCountMatrix(): void
+    {
+        $this->transactionsCountMatrix->reset();
     }
 }
